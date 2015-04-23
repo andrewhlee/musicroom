312b62912f745eca619070d26f16e3f9a472525e
Andrew Lee <ahyunlee@gmail.com>
2015-04-23 14:04:16 -0400

    howler in custom js

--- js/custom.js
+++ js/custom.js
@@ -1,23 +1,6 @@
 (function($) {
     $(document).ready(function() {
 
-    /* IF YOU WANT TO APPLY SOME BASIC JQUERY TO REMOVE THE VIDEO BACKGROUND ON A SPECIFIC VIEWPORT MANUALLY
-
-     var is_mobile = false;
-
-    if( $('.player').css('display')=='none') {
-        is_mobile = true;       
-    }
-    if (is_mobile == true) {
-        //Conditional script here
-        $('.big-background, .small-background-section').addClass('big-background-default-image');
-    }else{
-        $(".player").mb_YTPlayer(); 
-    }
-
-    });
-*/
-
     /*  IF YOU WANT TO USE DEVICE.JS TO DETECT THE VIEWPORT AND MANIPULATE THE OUTPUT  */
 
         //Device.js will check if it is Tablet or Mobile - http://matthewhudson.me/projects/device.js/
@@ -26,6 +9,11 @@
         } else {
             //jQuery will add the default background to the preferred class 
             $('.big-background').addClass('big-background-default-image');
+            var sound = new Howl ({
+                url:['sounds/sound1.mp3', 'sounds/sound1.ogg', 'sounds/sound1.aac'],
+                autoplay: true,
+                loop: true,
+            });
         }
     });
 
