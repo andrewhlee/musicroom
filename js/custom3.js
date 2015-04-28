b4b748c0b74175dd02c5db321bf82069df7dfd98
Andrew Lee <ahyunlee@gmail.com>
2015-04-28 14:45:40 -0400

    add howler to everything

---
+++ js/custom3.js
@@ -0,0 +1,18 @@
+(function($) {
+    $(document).ready(function() {
+
+    /*  IF YOU WANT TO USE DEVICE.JS TO DETECT THE VIEWPORT AND MANIPULATE THE OUTPUT  */
+
+        //Device.js will check if it is Tablet or Mobile - http://matthewhudson.me/projects/device.js/
+        if (!device.tablet() && !device.mobile()) {
+            $(".player").mb_YTPlayer();
+        } else {
+            //jQuery will add the default background to the preferred class 
+            $('.big-background').addClass('big-background-default-image');
+            var sound = new Howl({
+                urls:['andrew.mp3'],
+                loop: true
+            }).play();
+        }
+    });
+})(jQuery);
