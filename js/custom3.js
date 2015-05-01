2efac0a71737e017f2e7e3b356ac26760e6406ec
Andrew Lee <ahyunlee@gmail.com>
2015-05-01 17:20:34 -0400

    add scroll effect

--- js/custom3.js
+++ js/custom3.js
@@ -1,3 +1,14 @@
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1000, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
 (function($) {
     $(document).ready(function() {
 
